e21c337a2bd2c1d81773b2e231dd874a983d674f
DavRobert98 <davilarobert98@gmail.com>
2025-12-16 23:37:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import time
+import os
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 
-print("BOOT OK: el servicio arrancó correctamente")
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+session = os.environ["SESSION_STRING"]
 
-while True:
-    print("Servicio vivo...")
-    time.sleep(60)
+ORIGEN_GRUPO = int(os.environ["ORIGEN_GRUPO"])
+USUARIO_OBJETIVO = int(os.environ["USUARIO_OBJETIVO"])
+DESTINO_PERSONA = int(os.environ["DESTINO_PERSONA"])
+
+client = TelegramClient(StringSession(session), api_id, api_hash)
+
+@client.on(events.NewMessage(chats=ORIGEN_GRUPO))
+async def handler(event):
+    if event.sender_id != USUARIO_OBJETIVO:
+        return
+
+    await client.forward_messages(
+        entity=DESTINO_PERSONA,
+        messages=event.message
+    )
+
+async def main():
+    await client.start()
+    print("✅ Userbot activo 24/7 en Render")
+    await client.run_until_disconnected()
+
+client.loop.run_until_complete(main())
