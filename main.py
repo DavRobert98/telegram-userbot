250346524e20b55bfada07fa8b4e41f46e7c98b7
DavRobert98 <davilarobert98@gmail.com>
2025-12-16 21:49:09 -0600

    test railway running

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from telethon import TelegramClient, events
-import os
 
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
-
-client = TelegramClient("userbot", api_id, api_hash)
-
-@client.on(events.NewMessage)
-async def handler(event):
-    print("CHAT ID:", event.chat_id)
-
-client.start()
-client.run_until_disconnected()
